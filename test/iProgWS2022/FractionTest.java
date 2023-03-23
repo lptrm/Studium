2d4933a39cae38887c8ce0f2a80cef559a46b552
jan <j4n.obernberger@gmail.com>
2023-03-23 20:12:26 +0100

    implemented random testing algorithm in testIntValue, TODO: outsource mechanism for other units

--- test/iProgWS2022/FractionTest.java
+++ test/iProgWS2022/FractionTest.java
@@ -1,6 +1,10 @@
 package iProgWS2022;
 
 import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FractionTest {
@@ -38,11 +42,30 @@ public class FractionTest {
 
     @Test
     public void testIntValue() {
-        var frac = new Fraction(1,2);
-        assertEquals(0, frac.intValue());
+        //Bereichsgrenzen
+        Fraction[] fracB = { new Fraction(BigInteger.ONE,BigInteger.TWO), new Fraction(BigInteger.valueOf(Integer.MIN_VALUE),BigInteger.ONE),
+        new Fraction(BigInteger.valueOf(Integer.MAX_VALUE), BigInteger.ONE), new Fraction(BigInteger.ZERO, BigInteger.ZERO)};
+        //Randomisiert
+        Fraction[] fracR = new Fraction[100];
+        int[] refR = new int[100];
+        int i = 0;
+        do {
+            Random r1 = new Random();
+            Random r2 = new Random();
+            BigInteger numerator = i % 2 == 0 ? new BigInteger(31, r1) : new BigInteger(31, r1).negate();
+            BigInteger denominator = new BigInteger(31, r2);
+            int res = numerator.divide(denominator).intValue();
+            refR[i] = res;
+            fracR[i++] = new Fraction(numerator, denominator);
+        } while (i < 100);
+        i = 0;
+        do {
+            assertEquals(refR[i], fracR[i++].intValue());
+        } while (i < 100);
+        //assertEquals(0, frac.intValue());
 
-        frac = new Fraction(1, 1);
-        assertEquals(1, frac.intValue());
+        //frac = new Fraction(1, 1);
+        //assertEquals(1, frac.intValue());
     }
 /*
     @Test
