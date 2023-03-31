770103f67450376665d6726f30b88e98f98e6f67
jan <j4n.obernberger@gmail.com>
2023-03-31 14:02:40 +0200

    task 3

--- src/kProgSS2023/Rainbow.java
+++ src/kProgSS2023/Rainbow.java
@@ -1,6 +1,8 @@
 package kProgSS2023;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Rainbow extends Frame implements ActionListener{
     static Color[] colors = {Color.black, Color.blue, Color.cyan, Color.gray, Color.green, Color.magenta, Color.pink,
@@ -20,18 +22,32 @@ public class Rainbow extends Frame implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getActionCommand().equals("epilepsy")){
-            new Thread(){
-                @Override
-                public void run() {
-                    super.run();
-                }
-                Frame frame = new Frame(){
-                    Color[] colors = {Color.black, Color.blue, Color.cyan, Color.gray, Color.green, Color.magenta, Color.pink,
-                            Color.red, Color.white, Color.yellow};
-                    int iColor = 0;
+            new Frame() {
+                Timer timer = new Timer();
+                int iColor = 0;
 
-                };
-            };
+                void strobe() {
+                    timer.scheduleAtFixedRate(new TimerTask() {
+                        @Override
+                        public void run() {
+                            setBackground(colors[((++iColor%colors.length-1))]);
+                            repaint();
+                        }
+                    }, 1000, 1000);
+                    setSize(400, 150);
+                    setVisible(true);
+                }
+
+            }.strobe();
+
+
         }
+    }
+
+    public static void main(String[] args) {
+        Rainbow rainbow = new Rainbow();
+        rainbow.addWindowListener(new WindowQuitter());
+        rainbow.setSize(400, 150);
+        rainbow.setVisible(true);
     }
 }
